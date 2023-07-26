5010b5fa7fc513f78719d5c4140fa0d246e3a53f
phoenix6388 <phoenix6388@gmail.com>
2023-07-26 12:03:18 +0800

    add WLAN function

--- serial_bus.py
+++ serial_bus.py
@@ -1,6 +1,7 @@
 from machine import UART, Pin
 from time import sleep_ms
 import _thread
+import network
 from e34_2g4d20d_driver import E34_2G4D20D_Class
 
 uart1 = UART(1, baudrate=9600, tx=42, rx=41)
@@ -9,6 +10,19 @@ addr1 = Pin(21, Pin.IN)
 addr2 = Pin(14, Pin.IN)
 addr3 = Pin(2, Pin.IN)
 addr4 = Pin(1, Pin.IN)
+wlan = network.WLAN(network.STA_IF)
+
+def wifi_connect():
+    global wlan
+    wlan.active(True)
+    wlan.config(hostname='TMR Table')
+    if not wlan.isconnected():
+        print('connecting to network...')
+        print(wlan.scan())
+        wlan.connect('Goke2.4', 'goke56299299')
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
 
 def read_switch_addr():
     global addr1, addr2, addr3, addr4
@@ -22,32 +36,36 @@ def read_switch_addr():
     return switch_addr
     
 def uart1_read():
-    sleep_ms(3000)
-    print('#--begin reveive tmr data--#')
-    global uart1
-    while True:
-        msg_len = uart1.any()
-        if msg_len:
-            print("uart1_msg_len: " + str(msg_len))
-            msg = uart1.readline()
-            msg = msg.decode().split()
-            print("uart1_read msg: " + msg[0])
-            e34_2g4d20d_dev.uart2_dev.write(msg[0])
-        sleep_ms(10)
+    global wlan
+    if wlan.isconnected():
+        sleep_ms(10000)
+        print('#--begin reveive tmr data--#')
+        global uart1
+        while True:
+            msg_len = uart1.any()
+            if msg_len:
+                print("uart1_msg_len: " + str(msg_len))
+                msg = uart1.readline()
+                msg = msg.decode().split()
+                print("uart1_read msg: " + msg[0])
+                e34_2g4d20d_dev.uart2_dev.write(msg[0])
+            sleep_ms(10)
 
 _thread.start_new_thread(uart1_read, ())
 
 if __name__ == '__main__':
-    sleep_ms(500)
+    sleep_ms(1000)
+    wlan.disconnect()
     switch_addr = read_switch_addr()
     e34_2g4d20d_dev = E34_2G4D20D_Class()
-    e34_2g4d20d_dev.config_reg(baudrate=9600,addl=switch_addr,mode='Half_Duplex_Mode')         
+    e34_2g4d20d_dev.config_reg(baudrate=9600,addl=switch_addr,mode='Half_Duplex_Mode')
+    wifi_connect()
 
 #     sleep_ms(100)
 #     e34_2g4h20d_dev.read_version()
 #     e34_2g4h20d_dev.config_reg(baudrate=115200)
-    sleep_ms(500)
     print('#--program runing--#')
+    sleep_ms(500)
     while True:
         if (e34_2g4d20d_dev.uart2_dev.any()):
             data2 = e34_2g4d20d_dev.uart2_dev.read()
