254fb15d2bff74f89964ff1861ec3d14a8011cc4
phoenix6388 <phoenix6388@gmail.com>
2023-07-26 10:55:04 +0800

    uploading e34_2g4d20d_driver.py
    1.uploading e34_2g4d20d_driver.py；
    2.add read_switch_addr function.

---
+++ e34_2g4d20d_driver.py
@@ -0,0 +1,98 @@
+from machine import UART, Pin
+from time import sleep_ms
+
+class E34_2G4D20D_Class:
+    """ E34-2G4D20D class, incloud configure function"""
+
+    def __init__(self, addh=0x00, addl=0x00, alise="E34-2G4D20D"):
+        # Initialization command
+        self.uart2_dev = UART(2, baudrate=9600, tx=43, rx=44)
+        self.m0 = Pin(38, Pin.OUT)
+        self.m1 = Pin(39, Pin.OUT)
+        self.aux = Pin(40, Pin.IN)
+        self.dev_head = 0xc0
+        self.dev_addh = addh
+        self.dev_addl = addl
+        self.dev_sped = 0x18
+        
+        # | 3,2,1,0 通信信道（00H~0BH,共计12个信道）
+        # | 定频模式（模式0）
+        # | 0~5信道对应频率  | 2400M + CHAN * 2M     |
+        # | 6~11信道对应频率 | 2508M + (CHAN-6) * 2M |
+        # | 跳频模式（模式1）
+        # | 0~11信道对应频率 | 2412M + CHAN * 2M     |
+        self.dev_chan = 0x00
+        self.dev_option = 0x00
+        self.dev_speed = {'baudrate_9600': 0x18, 'baudrate_57600': 0x30}
+        self.dev_model = ['Half_Duplex_Mode','Full_Duplex_Mode','Reserve_Mode','Set_Mode']
+        pass
+    
+    def model_sel(self, mode='Half_Duplex_Mode'):
+        if (mode == self.dev_model[0]):
+            self.m0.value(0)
+            self.m1.value(0)
+        elif (mode == self.dev_model[1]):
+            self.m0.value(1)
+            self.m1.value(0)
+        elif (mode == self.dev_model[2]):
+            self.m0.value(0)
+            self.m1.value(1)
+        elif (mode == self.dev_model[3]):
+            self.m0.value(1)
+            self.m1.value(1)
+
+    def config_reg(self, baudrate=9600, addh=0x00, addl=0x00, chan=0x00, mode='Half_Duplex_Mode'):
+        self.uart2_dev.init(baudrate = 9600)
+        self.read_reg()
+        
+        self.dev_addh = addh
+        self.dev_addl = addl
+        if (baudrate == 9600):
+            self.dev_sped = self.dev_speed['baudrate_9600']
+        elif (baudrate == 57600):
+            self.dev_sped = self.dev_speed['baudrate_57600']
+        self.dev_chan = chan
+        w_bytes_list = bytearray([self.dev_head,self.dev_addh,self.dev_addl,self.dev_sped,self.dev_chan,self.dev_option])
+        print("w_bytes_list: ",end="")
+        print(w_bytes_list)
+        self.uart2_dev.write(w_bytes_list)
+        sleep_ms(100)
+        r_bytes_list = self.uart2_dev.read()
+        print("r_bytes_list: ",end="")
+        print(r_bytes_list)
+        if (w_bytes_list == r_bytes_list):
+            print('#--config register success--#')
+        
+        self.uart2_dev.init(baudrate = baudrate)
+        self.model_sel(mode)
+        sleep_ms(2)
+
+    def read_reg(self):
+        while (self.aux.value() == 0):
+            sleep_ms(2)
+        self.model_sel('Set_Mode')
+        sleep_ms(2)
+        print(bytearray([0xc1,0xc1,0xc1]))
+        self.uart2_dev.write(bytearray([0xc1,0xc1,0xc1]))
+        sleep_ms(100)
+        print(self.uart2_dev.read())
+        
+    def read_version(self):
+        while (self.aux.value() == 0):
+            sleep_ms(2)
+        self.model_sel('Set_Mode')
+        sleep_ms(2)
+        print(bytearray([0xc3,0xc3,0xc3]))
+        self.uart2_dev.write(bytearray([0xc3,0xc3,0xc3]))
+        sleep_ms(100)
+        print(self.uart2_dev.read())
+        
+    def reset_dev(self):
+        while (self.aux.value() == 0):
+            sleep_ms(2)
+        self.model_sel('Set_Mode')
+        sleep_ms(2)
+        print('#--reset e34-2g4h20d--#')
+        self.uart2_dev.write(bytearray([0xc4,0xc4,0xc4]))
+        
+        
--- serial_bus.py
+++ serial_bus.py
@@ -1,14 +1,28 @@
-from machine import UART
+from machine import UART, Pin
 from time import sleep_ms
 import _thread
-from e34_2g4h20d_driver import E34_2G4H20D_Class
+from e34_2g4d20d_driver import E34_2G4D20D_Class
 
 uart1 = UART(1, baudrate=9600, tx=42, rx=41)
 # uart2 = UART(2, baudrate=115200, tx=43, rx=44)
+addr1 = Pin(21, Pin.IN)
+addr2 = Pin(14, Pin.IN)
+addr3 = Pin(2, Pin.IN)
+addr4 = Pin(1, Pin.IN)
 
-
+def read_switch_addr():
+    global addr1, addr2, addr3, addr4
+    a1 = addr1.value()
+    a2 = addr2.value()
+    a3 = addr3.value()
+    a4 = addr4.value()
+    # 将4个1位数据拼成1个字节
+    switch_addr = (a4 << 3) | (a3 << 2) | (a2 << 1) | a1
+    print("switch_addr = " + str(switch_addr))
+    return switch_addr
+    
 def uart1_read():
-    sleep_ms(2000)
+    sleep_ms(3000)
     print('#--begin reveive tmr data--#')
     global uart1
     while True:
@@ -18,15 +32,16 @@ def uart1_read():
             msg = uart1.readline()
             msg = msg.decode().split()
             print("uart1_read msg: " + msg[0])
-            e34_2g4h20d_dev.uart2_dev.write(msg[0])
+            e34_2g4d20d_dev.uart2_dev.write(msg[0])
         sleep_ms(10)
 
 _thread.start_new_thread(uart1_read, ())
 
 if __name__ == '__main__':
     sleep_ms(500)
-    e34_2g4h20d_dev = E34_2G4H20D_Class()
-    e34_2g4h20d_dev.config_reg(baudrate=9600,mode='Fixed_Frequency_Mode')         
+    switch_addr = read_switch_addr()
+    e34_2g4d20d_dev = E34_2G4D20D_Class()
+    e34_2g4d20d_dev.config_reg(baudrate=9600,addl=switch_addr,mode='Half_Duplex_Mode')         
 
 #     sleep_ms(100)
 #     e34_2g4h20d_dev.read_version()
@@ -34,9 +49,9 @@ if __name__ == '__main__':
     sleep_ms(500)
     print('#--program runing--#')
     while True:
-        if (e34_2g4h20d_dev.uart2_dev.any()):
-            data2 = e34_2g4h20d_dev.uart2_dev.read()
-#             print(data2)
+        if (e34_2g4d20d_dev.uart2_dev.any()):
+            data2 = e34_2g4d20d_dev.uart2_dev.read()
+            print(data2)
 #             print("uart2_read msg: " + data2.decode())
         sleep_ms(10)
         
